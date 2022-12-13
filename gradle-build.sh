d0108fa349b6a656df99dd8c43d5407c80ff8f95
dongkyunkimdev <ddkds66@gmail.com>
2022-12-13 19:20:21 +0900

    docker image를 사용한 gradle build 소요시간이 크게 발생하여 gradle wrapper로 build하는 shell script 작성

---
+++ gradle-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+cd config-server && ./gradlew bootJar && cd ../
+cd user-service && ./gradlew bootJar && cd ../
+cd product-service && ./gradlew bootJar && cd ../
+cd order-service && ./gradlew bootJar && cd ../
+cd delivery-service && ./gradlew bootJar && cd ../
