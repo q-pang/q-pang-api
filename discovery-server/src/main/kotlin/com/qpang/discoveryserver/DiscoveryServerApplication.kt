9c24317b909ea683d9a11751861c66416e64c8a6
dongkyunkimdev <ddkds66@gmail.com>
2022-12-14 08:58:27 +0900

    eureka 서버 구성

--- discovery-server/src/main/kotlin/com/qpang/discoveryserver/DiscoveryServerApplication.kt
+++ discovery-server/src/main/kotlin/com/qpang/discoveryserver/DiscoveryServerApplication.kt
@@ -2,8 +2,10 @@ package com.qpang.discoveryserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class DiscoveryServerApplication
 
 fun main(args: Array<String>) {
