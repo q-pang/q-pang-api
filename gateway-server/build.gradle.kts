05495c1ce0788b40c7a08d94d4774e7c7cf17a6b
dongkyunkimdev <ddkds66@gmail.com>
2022-12-14 10:31:14 +0900

    gateway-server eureka, actuator 의존성 추가 및 마이크로서비스 routes 구성

--- gateway-server/build.gradle.kts
+++ gateway-server/build.gradle.kts
@@ -19,7 +19,9 @@ extra["springCloudVersion"] = "2021.0.5"
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
