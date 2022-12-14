16aae2b83c32ac447d2a3a8550852c9065e8e7ca
dongkyunkimdev <ddkds66@gmail.com>
2022-12-14 09:54:43 +0900

    discovery-server Dockerfile, gradle-build.sh 및 docker-compose 작성

--- gradle-build.sh
+++ gradle-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/zsh
 
 cd config-server && ./gradlew bootJar && cd ../
+cd discovery-server && ./gradlew bootJar && cd ../
 cd user-service && ./gradlew bootJar && cd ../
 cd product-service && ./gradlew bootJar && cd ../
 cd order-service && ./gradlew bootJar && cd ../
