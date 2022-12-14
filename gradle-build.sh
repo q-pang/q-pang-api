b71729f2737dc83080fdccdc56e81179a6ed36a7
dongkyunkimdev <ddkds66@gmail.com>
2022-12-14 10:44:48 +0900

    gateway-server gradle-build.sh, docker-compose 추가

--- gradle-build.sh
+++ gradle-build.sh
@@ -2,6 +2,7 @@
 
 cd config-server && ./gradlew bootJar && cd ../
 cd discovery-server && ./gradlew bootJar && cd ../
+cd gateway-server && ./gradlew bootJar && cd ../
 cd user-service && ./gradlew bootJar && cd ../
 cd product-service && ./gradlew bootJar && cd ../
 cd order-service && ./gradlew bootJar && cd ../
