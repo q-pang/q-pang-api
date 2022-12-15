95e085af41b7c0c8344a3579ee870020d776ba23
dongkyunkimdev <ddkds66@gmail.com>
2022-12-16 03:22:51 +0900

    ulid library 추가 및 PKEntity, JpaAuditEntity 작성

---
+++ user-service/src/main/kotlin/com/qpang/userservice/common/entity/PrimaryKeyEntity.kt
@@ -0,0 +1,40 @@
+package com.qpang.userservice.common.entity
+
+import com.github.f4b6a3.ulid.UlidCreator
+import org.hibernate.proxy.HibernateProxy
+import java.io.Serializable
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class PrimaryKeyEntity {
+    @Id
+    @Column(name = "id")
+    private val id: UUID = UlidCreator.getMonotonicUlid().toUuid()
+
+    fun getId(): UUID = id
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) {
+            return false
+        }
+
+        if (other !is HibernateProxy && this::class != other::class) {
+            return false
+        }
+
+        return id == getIdentifier(other)
+    }
+
+    private fun getIdentifier(obj: Any): Serializable {
+        return if (obj is HibernateProxy) {
+            obj.hibernateLazyInitializer.identifier
+        } else {
+            (obj as PrimaryKeyEntity).id
+        }
+    }
+
+    override fun hashCode() = Objects.hashCode(id)
+}
---
+++ user-service/src/main/kotlin/com/qpang/userservice/config/JpaAuditConfig.kt
@@ -0,0 +1,9 @@
+package com.qpang.userservice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditConfig {
+}
