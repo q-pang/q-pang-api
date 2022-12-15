5b00c85caa6e2312485116af4433e9a878fc74d3
dongkyunkimdev <ddkds66@gmail.com>
2022-12-16 07:39:26 +0900

    username이 중복되는 회원가입 테스트 케이스 추가

--- user-service/src/test/kotlin/com/qpang/userservice/application/service/SignupServiceDescribeSpec.kt
+++ user-service/src/test/kotlin/com/qpang/userservice/application/service/SignupServiceDescribeSpec.kt
@@ -2,6 +2,9 @@ package com.qpang.userservice.application.service
 
 import com.qpang.userservice.application.port.`in`.usecase.SignupUseCase
 import com.qpang.userservice.application.port.out.persistence.UserPersistencePort
+import com.qpang.userservice.application.service.exception.DuplicateUsernameException
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -12,18 +15,34 @@ class SignupServiceDescribeSpec : DescribeSpec({
     val signupService = SignupService(mockUserPersistencePort)
 
     describe("signup") {
-        context("올바른 Command가 주어지면") {
-            it("회원가입에 성공하고 SignupInfo 응답") {
-                every { mockUserPersistencePort.save(any()) } answers { correctAnySignupCommand.toEntity() }
-                val signupInfo = signupService.signup(correctAnySignupCommand)
+        every { mockUserPersistencePort.save(any()) } answers { anySignupCommand.toEntity() }
 
-                signupInfo.name shouldBe correctAnySignupCommand.name
+        context("회원가입되지 않은 username을 가진 Command가 주어지면") {
+            every { mockUserPersistencePort.existsByUsername(anySignupCommand.username) } answers { false }
+            it("회원가입에 성공하고 SignupInfo 응답") {
+                val signupInfo = signupService.signup(anySignupCommand)
+
+                assertSoftly {
+                    signupInfo.username shouldBe anySignupCommand.username
+                    signupInfo.name shouldBe anySignupCommand.name
+                }
+            }
+        }
+
+        context("이미 회원가입된 username을 가진 Command가 주어지면") {
+            every { mockUserPersistencePort.existsByUsername(anySignupCommand.username) } answers { true }
+            it("DuplicateUsernameException 발생") {
+                shouldThrow<DuplicateUsernameException> {
+                    signupService.signup(anySignupCommand)
+                }
             }
         }
     }
 }) {
     companion object {
-        private val correctAnySignupCommand = SignupUseCase.SignupCommand(
+        private val anySignupCommand = SignupUseCase.SignupCommand(
+            username = "username",
+            password = "password",
             name = "name"
         )
     }
