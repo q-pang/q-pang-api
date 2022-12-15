dc26dddd2a5950e4e74e0d82b81bb2cea0980b56
dongkyunkimdev <ddkds66@gmail.com>
2022-12-16 06:51:10 +0900

    Http Response로 ResponseEntity 객체 사용

--- user-service/src/main/kotlin/com/qpang/userservice/adapter/in/rest/SignupRestAdapter.kt
+++ user-service/src/main/kotlin/com/qpang/userservice/adapter/in/rest/SignupRestAdapter.kt
@@ -2,6 +2,7 @@ package com.qpang.userservice.adapter.`in`.rest
 
 import com.qpang.userservice.application.port.`in`.usecase.SignupUseCase
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -15,8 +16,8 @@ class SignupRestAdapter(
 ) {
     @PostMapping("/user/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    fun signup(@RequestBody @Valid dto: SignupRequestDto): SignupResponseDto =
-        SignupResponseDto.from(signupUseCase.signup(dto.toCommand()))
+    fun signup(@RequestBody @Valid dto: SignupRequestDto): ResponseEntity<SignupResponseDto> =
+        ResponseEntity(SignupResponseDto.from(signupUseCase.signup(dto.toCommand())), HttpStatus.CREATED)
 
     data class SignupRequestDto(val name: String) {
         fun toCommand(): SignupUseCase.SignupCommand = SignupUseCase.SignupCommand(name = this.name)
