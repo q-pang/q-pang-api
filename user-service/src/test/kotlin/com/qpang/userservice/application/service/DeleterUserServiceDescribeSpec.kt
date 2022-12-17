0dc81e78b2a50901b8ee19833dd042907996d3b3
dongkyunkimdev <ddkds66@gmail.com>
2022-12-18 08:17:09 +0900

    회원탈퇴 Application Service 단위 테스트 케이스 작성

---
+++ user-service/src/test/kotlin/com/qpang/userservice/application/service/DeleterUserServiceDescribeSpec.kt
@@ -0,0 +1,47 @@
+package com.qpang.userservice.application.service
+
+import com.qpang.userservice.application.port.`in`.usecase.DeleteUserUseCase
+import com.qpang.userservice.application.port.out.persistence.UserPersistencePort
+import com.qpang.userservice.application.service.exception.UsernameNotFoundException
+import com.qpang.userservice.domain.User
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+class DeleterUserServiceDescribeSpec : DescribeSpec({
+    val mockUserPersistencePort: UserPersistencePort = mockk()
+    val deleteUserService = DeleteUserService(mockUserPersistencePort)
+
+    describe("deleteUser") {
+        context("회원가입된 username을 가진 Command가 주어지면") {
+            val expectedUser = User(username = "username", password = "password", name = "name")
+            every { mockUserPersistencePort.findByUsername(anyDeleteUserCommand.username) } answers { expectedUser }
+            every { mockUserPersistencePort.delete(expectedUser) } answers {}
+            it("회원탈퇴에 성공하고 DeleteUserInfo 응답") {
+                val deleteUserInfo = deleteUserService.command(anyDeleteUserCommand)
+
+                assertSoftly {
+                    deleteUserInfo.username shouldBe anyDeleteUserCommand.username
+                }
+            }
+        }
+
+        context("회원가입되지 않은 username을 가진 Command가 주어지면") {
+            every { mockUserPersistencePort.findByUsername(anyDeleteUserCommand.username) } answers { null }
+            it("UsernameNotFoundException 발생") {
+                shouldThrow<UsernameNotFoundException> {
+                    deleteUserService.command(anyDeleteUserCommand)
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        private val anyDeleteUserCommand = DeleteUserUseCase.DeleteUserCommand(
+            username = "username"
+        )
+    }
+}
