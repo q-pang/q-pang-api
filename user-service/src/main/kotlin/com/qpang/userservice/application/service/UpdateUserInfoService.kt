fc07483f39f06200f21a0ebeb3f030269079d8b9
dongkyunkimdev <ddkds66@gmail.com>
2022-12-18 06:23:06 +0900

    remove Usage 0 Class

--- user-service/src/main/kotlin/com/qpang/userservice/application/service/UpdateUserInfoService.kt
+++ user-service/src/main/kotlin/com/qpang/userservice/application/service/UpdateUserInfoService.kt
@@ -2,7 +2,6 @@ package com.qpang.userservice.application.service
 
 import com.qpang.userservice.application.port.`in`.usecase.UpdateUserInfoUseCase
 import com.qpang.userservice.application.port.out.persistence.UserPersistencePort
-import com.qpang.userservice.application.service.exception.UserIdNotFoundException
 import com.qpang.userservice.application.service.exception.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
--- user-service/src/main/kotlin/com/qpang/userservice/application/service/exception/UserIdNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.qpang.userservice.application.service.exception
-
-import com.qpang.userservice.common.exception.BusinessException
-
-class UserIdNotFoundException(userId: String) : BusinessException(userId) {
-    override val message = "User ID가 존재하지 않습니다. : $userId"
-}
