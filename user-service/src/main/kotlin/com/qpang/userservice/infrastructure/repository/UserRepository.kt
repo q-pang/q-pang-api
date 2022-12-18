23325dedec90fcfbd8485f36a46b21ad217ee299
dongkyunkimdev <ddkds66@gmail.com>
2022-12-19 04:41:43 +0900

    UserService: User JpaRepository PK Type 변경

--- user-service/src/main/kotlin/com/qpang/userservice/infrastructure/repository/UserRepository.kt
+++ user-service/src/main/kotlin/com/qpang/userservice/infrastructure/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package com.qpang.userservice.infrastructure.repository
 import com.qpang.userservice.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Int> {
+interface UserRepository : JpaRepository<User, String> {
     fun existsByUsername(username: String): Boolean
     fun findByUsername(username: String): User?
 }
