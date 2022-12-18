40a115fc098fcba7ef4859aa6646044b2628ae7e
dongkyunkimdev <ddkds66@gmail.com>
2022-12-19 04:55:55 +0900

    Gateway Server: JwtValidateException 401 http status 변경

--- gateway-server/src/main/kotlin/com/qpang/gatewayserver/exception/JwtValidateException.kt
+++ gateway-server/src/main/kotlin/com/qpang/gatewayserver/exception/JwtValidateException.kt
@@ -1,5 +1,9 @@
 package com.qpang.gatewayserver.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "invalid token")
 class JwtValidateException(token: String) : BusinessException(token) {
     override val message = "token 검증에 실패하였습니다. : $token"
 }
