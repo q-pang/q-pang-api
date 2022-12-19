f14ccf556bc9eb17d85f1019e4ff24165d44a6cc
dongkyunkimdev <ddkds66@gmail.com>
2022-12-20 08:51:46 +0900

    OrderService: config Jpa Auditing

---
+++ order-service/src/main/kotlin/com/qpang/orderservice/config/JpaAuditConfig.kt
@@ -0,0 +1,9 @@
+package com.qpang.orderservice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditConfig {
+}
