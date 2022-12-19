c4f5d40f727f291ba983915fbd2377e11ab7bad8
dongkyunkimdev <ddkds66@gmail.com>
2022-12-20 03:48:21 +0900

    ProductService: 상품 조회 Application Service Transaction readonly true 적용

--- product-service/src/main/kotlin/com/qpang/productservice/application/service/GetProductService.kt
+++ product-service/src/main/kotlin/com/qpang/productservice/application/service/GetProductService.kt
@@ -4,11 +4,13 @@ import com.qpang.productservice.application.port.`in`.usecase.GetProductUseCase
 import com.qpang.productservice.application.port.out.persistence.ProductPersistencePort
 import com.qpang.productservice.application.service.exception.ProductIdNotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class GetProductService(
     private val productPersistencePort: ProductPersistencePort
 ) : GetProductUseCase {
+    @Transactional(readOnly = true)
     override fun command(command: GetProductUseCase.GetProductCommand): GetProductUseCase.GetProductInfo {
         val savedProduct = productPersistencePort.findProductById(command.id)
         savedProduct ?: throw ProductIdNotFoundException(command.id)
