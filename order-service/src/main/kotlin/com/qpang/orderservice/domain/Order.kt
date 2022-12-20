0c7c0d9f2bb4512089dd6e45f3e6c3da6a34dc06
dongkyunkimdev <ddkds66@gmail.com>
2022-12-20 09:52:33 +0900

    OrderService: Order - OrderItem, Order - Payment 양방향 매핑 구성

--- order-service/src/main/kotlin/com/qpang/orderservice/domain/Order.kt
+++ order-service/src/main/kotlin/com/qpang/orderservice/domain/Order.kt
@@ -1,15 +1,15 @@
 package com.qpang.orderservice.domain
 
 import com.qpang.orderservice.common.entity.JpaAuditEntity
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "orders")
 class Order(
     totalPrice: Long,
     consumerId: String,
+    orderItems: MutableList<OrderItem>,
+    payment: Payment
 ) : JpaAuditEntity() {
     @Column(name = "total_price", nullable = false)
     var totalPrice: Long = totalPrice
@@ -17,5 +17,13 @@ class Order(
 
     @Column(name = "consumer_id", nullable = false)
     var consumerId: String = consumerId
+        protected set
+
+    @OneToMany(mappedBy = "order", cascade = [CascadeType.PERSIST], orphanRemoval = true)
+    var orderItems: MutableList<OrderItem> = orderItems
+        protected set
+
+    @OneToOne(mappedBy = "order", optional = false, cascade = [CascadeType.PERSIST], orphanRemoval = true)
+    var payment: Payment = payment
         protected set
 }
