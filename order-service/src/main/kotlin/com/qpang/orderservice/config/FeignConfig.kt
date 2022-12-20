9dd4e346bdb225a8d44b0a372136a07bb19ec918
dongkyunkimdev <ddkds66@gmail.com>
2022-12-20 23:23:09 +0900

    OrderServie: add openfeign dependency and config openfeign

---
+++ order-service/src/main/kotlin/com/qpang/orderservice/config/FeignConfig.kt
@@ -0,0 +1,9 @@
+package com.qpang.orderservice.config
+
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableFeignClients
+class FeignConfig {
+}
