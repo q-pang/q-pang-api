3fbbcf1dde98bf6725b5f396f6174bd6103c7aaf
dongkyunkimdev <ddkds66@gmail.com>
2022-12-21 01:40:02 +0900

    OrderService: openfeign client name을 유레카 서버에 등록된 이름으로 변경 및 SpringQueryMap에 ids 담기

--- order-service/src/main/kotlin/com/qpang/orderservice/infrastructure/feignclient/ProductServiceClient.kt
+++ order-service/src/main/kotlin/com/qpang/orderservice/infrastructure/feignclient/ProductServiceClient.kt
@@ -2,10 +2,11 @@ package com.qpang.orderservice.infrastructure.feignclient
 
 import com.qpang.orderservice.application.port.out.rest.dto.ProductResponseDto
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.cloud.openfeign.SpringQueryMap
 import org.springframework.web.bind.annotation.GetMapping
 
-@FeignClient(name = "productServiceClient")
+@FeignClient(name = "product-service")
 interface ProductServiceClient {
     @GetMapping("/product/list/ids")
-    fun getProductListIds(productIds: List<String>): List<ProductResponseDto>
+    fun getProductListIds(@SpringQueryMap productIds: List<String>): List<ProductResponseDto>
 }
