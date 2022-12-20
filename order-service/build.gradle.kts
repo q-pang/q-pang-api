2c7e64049374e56d8c9423892235ef14b9d12dec
dongkyunkimdev <ddkds66@gmail.com>
2022-12-20 11:09:37 +0900

    OrderService: config springdoc openapi swagger-ui

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("mysql:mysql-connector-java")
 	implementation("com.github.f4b6a3:ulid-creator:5.1.0")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotest:kotest-runner-junit5-jvm:${property("kotestVersion")}")
