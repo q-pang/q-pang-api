bc58e2ab96da951ed767232d73690d22e54ba403
dongkyunkimdev <ddkds66@gmail.com>
2022-12-22 07:41:31 +0900

    OrderService: PaymentModuel Componet annotation 추가

--- order-service/src/main/kotlin/com/qpang/orderservice/infrastructure/external/PaymentModule.kt
+++ order-service/src/main/kotlin/com/qpang/orderservice/infrastructure/external/PaymentModule.kt
@@ -2,7 +2,9 @@ package com.qpang.orderservice.infrastructure.external
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 
+@Component
 class PaymentModule {
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
     fun payment(name: String, typeName: String, companyName: String, number: String, amount: Long) {
