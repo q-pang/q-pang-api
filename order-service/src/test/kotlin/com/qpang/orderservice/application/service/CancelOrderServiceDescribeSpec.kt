96716e09d0d0956b4b3617fa6ce2a31be41dad5e
dongkyunkimdev <ddkds66@gmail.com>
2022-12-22 10:30:35 +0900

    OrderService: 주문 취소 Application Service의 주문이 이미 취소 상태인 실패 단위 테스트 케이스 작성

--- order-service/src/test/kotlin/com/qpang/orderservice/application/service/CancelOrderServiceDescribeSpec.kt
+++ order-service/src/test/kotlin/com/qpang/orderservice/application/service/CancelOrderServiceDescribeSpec.kt
@@ -6,6 +6,8 @@ import com.qpang.orderservice.application.port.out.external.PaymentPort
 import com.qpang.orderservice.application.port.out.persistence.OrderPersistencePort
 import com.qpang.orderservice.application.port.out.rest.DeliveryServiceRestPort
 import com.qpang.orderservice.application.service.exception.OrderNotFoundException
+import com.qpang.orderservice.domain.Order
+import com.qpang.orderservice.domain.exception.AlreadyCanceledFoundException
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.mockk.every
@@ -20,6 +22,20 @@ class CancelOrderServiceDescribeSpec : DescribeSpec({
         CancelOrderService(mockOrderPersistencePort, mockPaymentPort, mockEventProducePort, mockDeliveryServiceRestPort)
 
     describe("cancelOrder") {
+        context("등록된 orderId를 가지고") {
+            val expectedOrder = Order("registeredConsumerId")
+            every { mockOrderPersistencePort.findOrderById("registeredOrderId") } answers { expectedOrder }
+
+            context("주문이 이미 취소 상태이면") {
+                expectedOrder.cancel()
+                it("주문 취소에 실패하고 AlreadyCanceledFoundException 발생") {
+                    shouldThrow<AlreadyCanceledFoundException> {
+                        cancelOrderService.command(registeredOrderIdCommand)
+                    }
+                }
+            }
+        }
+
         context("등록되지 않은 orderId를 가진 커맨드가 주어지면") {
             every { mockOrderPersistencePort.findOrderById("notRegisteredOrderId") } answers { null }
             it("주문 취소에 실패하고 OrderNotFoundException 발생") {
@@ -31,6 +47,9 @@ class CancelOrderServiceDescribeSpec : DescribeSpec({
     }
 }) {
     companion object {
+        private val registeredOrderIdCommand: CancelOrderUseCase.CancelOrderCommand =
+            CancelOrderUseCase.CancelOrderCommand("registeredOrderId")
+
         private val notRegisteredOrderIdCommand: CancelOrderUseCase.CancelOrderCommand =
             CancelOrderUseCase.CancelOrderCommand("notRegisteredOrderId")
     }
