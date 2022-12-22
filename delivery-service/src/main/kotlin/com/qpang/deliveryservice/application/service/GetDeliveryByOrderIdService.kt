f917320a157c38dc0ecdae3918dc6c4b3d0e8d51
dongkyunkimdev <ddkds66@gmail.com>
2022-12-22 09:20:34 +0900

    DeliveryService: 주문 id로 배송 조회 Application Service 잘못된 패키지 변경

---
+++ delivery-service/src/main/kotlin/com/qpang/deliveryservice/application/service/GetDeliveryByOrderIdService.kt
@@ -0,0 +1,18 @@
+package com.qpang.deliveryservice.application.service
+
+import com.qpang.deliveryservice.application.port.`in`.GetDeliveryByOrderIdUseCase
+import com.qpang.deliveryservice.application.port.out.persistence.DeliveryPersistencePort
+import com.qpang.deliveryservice.application.service.exception.DeliveryIdNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class GetDeliveryByOrderIdService(
+    private val deliveryPersistencePort: DeliveryPersistencePort
+) : GetDeliveryByOrderIdUseCase {
+    override fun command(command: GetDeliveryByOrderIdUseCase.GetDeliveryByOrderIdCommand): GetDeliveryByOrderIdUseCase.GetDeliveryByOrderIdInfo {
+        val savedDelivery = deliveryPersistencePort.findByOrderId(command.orderId)
+        savedDelivery ?: throw DeliveryIdNotFoundException(command.orderId)
+
+        return GetDeliveryByOrderIdUseCase.GetDeliveryByOrderIdInfo.from(savedDelivery)
+    }
+}
